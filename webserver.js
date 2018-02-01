dfc1e467c70b505f26377e55577307b8e8ff6abb
itpstudent <itpstudent@CONFERENCE_RM.ITP.TSOA.NYU.EDU>
2018-02-01 14:24:40 -0500

    Created public static folder

--- webserver.js
+++ webserver.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'));
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -8,7 +11,6 @@ app.get('/', function (req, res) {
 app.get('/somethingelse', function (req, res) {
   res.send('<html><body><h1>Something Else</h1></body></html>')
 })
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
